8a52a9f759233619a729a50280983b73ee66e9af
chestnutbunker <kelvin_foo_13vin@hotmail.com>
2017-11-25 20:27:41 +0800

    Sliding up pane layout added

--- app/src/main/java/home/com/googlemap/MapsActivity.java
+++ app/src/main/java/home/com/googlemap/MapsActivity.java
@@ -66,7 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.sliding_layout);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             checkLocationPermission();
